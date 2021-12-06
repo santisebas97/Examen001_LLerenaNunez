db518258cc11c7170cba416678e08f39c333344a
santisebas97 <nunezsantiago1e@gmail.com>
2021-12-06 18:48:03 -0500

    commit vanesa llerena "18h47"

--- app/src/main/java/com/fisei/ejercicio01_nunezsantiago/MainActivity.java
+++ app/src/main/java/com/fisei/ejercicio01_nunezsantiago/MainActivity.java
@@ -3,12 +3,27 @@ package com.fisei.ejercicio01_nunezsantiago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText numeroA;
+    private EditText numeroB;
+    private TextView resultado;
+    private Button sumar;
+    private Button siguiente;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numeroA = findViewById(R.id.editTextNumeroA);
+        numeroB = findViewById(R.id.editTextNumeroB);
+        resultado = findViewById(R.id.textViewResultado);
+        sumar = findViewById(R.id.buttonSumar);
+        siguiente = findViewById(R.id.buttonSiguiente);
     }
 }
