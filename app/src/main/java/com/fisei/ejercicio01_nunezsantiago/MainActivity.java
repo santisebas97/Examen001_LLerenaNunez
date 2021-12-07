84d02c8bb602519fc408282f676f8f7c9de717f5
santisebas97 <nunezsantiago1e@gmail.com>
2021-12-06 19:06:15 -0500

    commit vanesa y santi "19h05"

--- app/src/main/java/com/fisei/ejercicio01_nunezsantiago/MainActivity.java
+++ app/src/main/java/com/fisei/ejercicio01_nunezsantiago/MainActivity.java
@@ -1,8 +1,11 @@
 package com.fisei.ejercicio01_nunezsantiago;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,4 +29,32 @@ public class MainActivity extends AppCompatActivity {
         sumar = findViewById(R.id.buttonSumar);
         siguiente = findViewById(R.id.buttonSiguiente);
     }
+
+    public void Siguiente(View view){
+        Intent intent = new Intent(this, SegundaActividad.class);
+        this.startActivity(intent);
+    }
+
+    public void Sumar(View view){
+            String numA = numeroA.getText().toString();
+        String numB = numeroB.getText().toString();
+
+        char[] A = numA.toCharArray();
+        char[] B = numB.toCharArray();
+
+
+        if(A.length==B.length){
+            int[] resultados=new int[A.length];
+            for(int i=0; i<=A.length;i++){
+
+                 resultados[i] = Integer.valueOf(A[1])+Integer.valueOf(B[i]);
+            }
+        }
+
+        //resultado.setText(String.valueOf());
+
+
+
+    }
+
 }
